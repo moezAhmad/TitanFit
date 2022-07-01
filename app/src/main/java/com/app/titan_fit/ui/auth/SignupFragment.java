cf9a5f9d70695b70b8e6a998466121887e61610c
moezAhmad <wyellow19@gmail.com>
2022-07-01 23:56:09 +0500

    Done with changes

--- app/src/main/java/com/app/titan_fit/ui/auth/SignupFragment.java
+++ app/src/main/java/com/app/titan_fit/ui/auth/SignupFragment.java
@@ -1,25 +1,15 @@
 package com.app.titan_fit.ui.auth;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,6 +30,7 @@ public class SignupFragment extends Fragment {
     private FirebaseAuth mAuth;
     private TextInputLayout email;
     private TextInputLayout password;
+    private TextInputLayout confirmPassword;
     private Button signup;
     private TextView toSignin;
 
@@ -53,6 +44,7 @@ public class SignupFragment extends Fragment {
         mAuth = FirebaseAuth.getInstance();
         email = binding.email;
         password = binding.password;
+        confirmPassword = binding.confirmPassword;
         signup = binding.signup;
         toSignin = binding.toSignin;
         signup.setOnClickListener(view -> {
@@ -66,6 +58,10 @@ public class SignupFragment extends Fragment {
             }
             if(this.password.getEditText().getText().toString().trim().length()<7){
                 password.setError("Length must be greater than 6");
+                return;
+            }
+            if(!this.password.getEditText().getText().toString().equals(this.confirmPassword.getEditText().getText().toString())){
+                confirmPassword.setError("Passwords don't match");
                 return;
             }
             mAuth.createUserWithEmailAndPassword(email.getEditText().getText().toString(), password.getEditText().getText().toString())
